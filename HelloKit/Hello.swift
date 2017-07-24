5a77f51e00042b481fc13a042fff312bf4f3f563
plumhly <plum@plums-MacBook-Pro.local>
2017-07-24 15:37:42 +0800

    package

---
+++ HelloKit/Hello.swift
@@ -0,0 +1,15 @@
+//
+//  Hello.swift
+//  Swift_Tips
+//
+//  Created by plum on 2017/7/21.
+//  Copyright © 2017年 sigma-td. All rights reserved.
+//
+
+import Foundation
+
+public class Hello {
+  public  class func sayHello() {
+        print("Hello Kit")
+    }
+}
---
+++ HelloKit/HelloKit.h
@@ -0,0 +1,19 @@
+//
+//  HelloKit.h
+//  HelloKit
+//
+//  Created by plum on 2017/7/21.
+//  Copyright © 2017年 sigma-td. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloKit.
+FOUNDATION_EXPORT double HelloKitVersionNumber;
+
+//! Project version string for HelloKit.
+FOUNDATION_EXPORT const unsigned char HelloKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloKit/PublicHeader.h>
+
+
