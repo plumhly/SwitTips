bd5b6e0219c46cfefa2aa3ca2559ce912c426704
Plum <215747586@qq.xom>
2017-07-24 22:54:44 +0800

    可视化

---
+++ Swift_Tips/Playgrounds/Playground 可视化开发.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+/*
+let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+label.text = "hello libo"
+label.backgroundColor = UIColor.red
+PlaygroundPage.current.liveView = label
+*/
+
+class ViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected: \(indexPath.row)")
+    }
+}
+
+let vc = ViewController()
+PlaygroundPage.current.liveView = vc
